b85bc73a2dc3d9e9c241830947a9a3cd693aabff
pjvilloud <pjvilloud@protonmail.com>
2020-03-24 15:00:00 +0100

    Reinit

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -48,10 +48,7 @@ public class Employe {
      * @return
      */
     public Integer getNombreAnneeAnciennete() {
-        if(dateEmbauche != null && dateEmbauche.isBefore(LocalDate.now())){
-            return LocalDate.now().getYear() - dateEmbauche.getYear();
-        }
-        return 0;
+        return LocalDate.now().getYear() - dateEmbauche.getYear();
     }
 
     public Integer getNbConges() {
@@ -63,22 +60,13 @@ public class Employe {
     }
 
     public Integer getNbRtt(LocalDate d){
-        int i1 = d.isLeapYear() ? 365 : 366;
-        int var = 104;
+        int i1 = d.isLeapYear() ? 365 : 366;int var = 104;
         switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-            case THURSDAY:
-                if(d.isLeapYear()) {
-                    var =  var + 1;
-                }
-                break;
-            case FRIDAY:
-                if(d.isLeapYear()) {
-                    var =  var + 2;
-                } else {
-                    var =  var + 1;
-                }
-                case SATURDAY:
-                    var = var + 1;
+        case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
+        case FRIDAY:
+        if(d.isLeapYear()) var =  var + 2;
+        else var =  var + 1;
+case SATURDAY:var = var + 1;
                     break;
         }
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate ->
--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -59,7 +59,6 @@ public class EmployeService {
         if(tempsPartiel != null){
             salaire = salaire * tempsPartiel;
         }
-        salaire = Math.round(salaire*100d)/100d;
 
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
