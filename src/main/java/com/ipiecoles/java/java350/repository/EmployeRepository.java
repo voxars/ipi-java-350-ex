d806b4112a47eedfc8b18f1de0eb58d17af28af0
Arthur Montesinos <60731021+voxars@users.noreply.github.com>
2022-04-26 16:18:57 +0200

    -

--- src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
